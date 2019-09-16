6ffff9a3fcdb76d542554a9801abcd4e806fe703
Wojtek <Wojtek@DESKTOP-98S186O>
2019-09-16 13:33:32 +0200

    new

---
+++ dd/src/main/java/ss/dd/App.java
@@ -0,0 +1,14 @@
+package ss.dd;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
